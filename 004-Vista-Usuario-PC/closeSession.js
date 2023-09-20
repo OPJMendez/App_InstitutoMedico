08ef2697512dd117aa3dde891f7352ce90c5c81c
ArthurPain <o.p.logisticojjmendez@gmail.com>
2023-09-19 21:11:24 -0500

    feat

---
+++ 004-Vista-Usuario-PC/closeSession.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // Obtén referencias a los elementos HTML
+  var closeSession = document.getElementById('btn-Icon-User');
+  var closeSessionWindow = document.getElementById('message-card-two');
+  var closeMessage = document.getElementById('btn-CloseMessage');
+  closeSession.addEventListener('click', function () {
+    closeSessionWindow.style.display = 'block';
+  });
+  closeMessage.addEventListener('click', function () {
+    closeSessionWindow.style.display = 'none';
+  });
+});
